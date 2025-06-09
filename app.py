483b1bf7c291775091f412074bc97cad1d929f74
Savinup21 <savinu083@gmail.com>
2025-06-09 13:00:24 -0400

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import tensorflow as tf
 import numpy as np
 from PIL import Image
 
-st.set_page_config(page_title="X-Ray Fracture Detection", layout="centered")
+st.set_page_config(page_title="MRI Fracture Detection", layout="centered")
 
 IMG_SIZE = (224, 224)
 CLASS_LABELS = ['Fracture was Detected', 'No Fracture Detected']
@@ -38,9 +38,9 @@ def predict_with_tflite(interpreter, image_array):
 
 interpreter = load_tflite_model()
 
-st.title("X-Ray Bone Fracture Detection with AI")
+st.title("MRI Bone Fracture Detection with AI")
 
-uploaded_file = st.file_uploader("Upload an X-ray Image", type=['jpg', 'jpeg', 'png'])
+uploaded_file = st.file_uploader("Upload an MRI Image", type=['jpg', 'jpeg', 'png'])
 
 if uploaded_file:
     pil_image = Image.open(uploaded_file).convert('RGB')
@@ -49,8 +49,7 @@ if uploaded_file:
     # Make prediction
     output = predict_with_tflite(interpreter, image_input)
 
-    # Check output shape and apply appropriate logic
-    if output.shape[-1] == 1:  # Sigmoid output (binary classification)
+    if output.shape[-1] == 1: 
         prob = output[0][0]
         predicted_class_index = 1 if prob < 0.5 else 0
         confidence = (1 - prob) * 100 if predicted_class_index == 1 else prob * 100
@@ -59,9 +58,9 @@ if uploaded_file:
         predicted_class_index = int(np.argmax(probabilities))
         confidence = probabilities[predicted_class_index] * 100
 
-    st.image(pil_image, caption="📷 Uploaded X-ray Image", use_container_width=True)
+    st.image(pil_image, caption="📷 Uploaded MRI Image", use_container_width=True)
     st.success(f"Prediction: **{CLASS_LABELS[predicted_class_index]}** ({confidence:.2f}%)")
 else:
-    st.info("⬆️ Please upload an X-ray image to begin.")
+    st.info("⬆️ Please upload an MRI image to begin.")
 
 
